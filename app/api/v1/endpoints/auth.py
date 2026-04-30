01ad0ff4cce4402bd4df4d3ca6c85d7ac5817624
Sabarinath E M <sabarinathem36@gmail.com>
2026-04-30 10:27:54 +0530

    feat: implement basic login API with dummy user validation

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 
 
 router = APIRouter()
 
+
+dummy_details = {'username':'sabarinathem','password':'Sabari@1996'}
+
 @router.post("/login")
-def login():
-    return "This is login"
+def login(username:str,password:str):
+    if username != dummy_details["username"] or password != dummy_details["password"]:
+        raise HTTPException(status_code=401,detail="Invalid credentials")
+    return {"message":"Login Successful"}
