210147659a9dd1709743fe624fd1dc39da3143e1
Sabarinath E M <sabarinathem36@gmail.com>
2026-04-30 10:06:19 +0530

    feat: add routing structure (main, router, endpoints)

---
+++ app/api/v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.api.v1.endpoints import *
+
+
+api_router = APIRouter()
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from app.api.v1.router import api_router
+
 app = FastAPI()
 
-@app.get("/home")
-def home():
-    return "This is a home page"
+app.include_router(api_router,prefix="/api/v1")
+
 
