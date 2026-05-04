404ecbe6163a581a05c73676b90981bcbfe7c765
Sabarinath E M <sabarinathem36@gmail.com>
2026-05-05 00:14:08 +0530

    feat: create a check-auth api in user.py for check the user is authenticated or not

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get("/profile")
 def profile(request:Request,user=Depends(get_current_user)):
     return {"user_email":user}
+
+
+@router.get("/check-auth")
+def check_auth(user=Depends(get_current_user)):
+    return user
